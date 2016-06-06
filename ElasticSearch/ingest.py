9e191dba7075f36bead49cd434d5673b5ff379bd
Christine <cszchung@gmail.com>
2016-06-06 14:05:27 -0500

    added comments

--- ElasticSearch/ingest.py
+++ ElasticSearch/ingest.py
@@ -4,7 +4,10 @@ import csv, time
 from datetime import datetime, timedelta, tzinfo
 
 class FixedOffset(tzinfo):
-    """Fixed offset in minutes: `time = utc_time + utc_offset`."""
+    """
+    Class to fix time offset during ingestion in minutes:
+    `time = utc_time + utc_offset`.
+    """
     def __init__(self, offset):
         self.__offset = timedelta(minutes=offset)
         hours, minutes = divmod(offset, 60)
@@ -12,27 +15,35 @@ class FixedOffset(tzinfo):
         #  that have the opposite sign in the name;
         #  the corresponding numeric value is not used e.g., no minutes
         self.__name = '<%+03d%02d>%+d' % (hours, minutes, -hours)
+
     def utcoffset(self, dt=None):
         return self.__offset
+
     def tzname(self, dt=None):
         return self.__name
+
     def dst(self, dt=None):
         return timedelta(0)
+
     def __repr__(self):
         return 'FixedOffset(%d)' % (self.utcoffset().total_seconds() / 60)
 
-# Define a default Elasticsearch client
-connections.create_connection(hosts=['localhost'])
 
-#we may still want to add building code into a separate column
+# Define a default Elasticsearch client
+#
+connections.create_connection(hosts=['http://ec2-52-37-208-115.us-west-2.compute.amazonaws.com'])
+
+
 class Reading(DocType):
     datetime = Date()
+    location = GeoPoint(lat_lon=True)
+    usage = Float()
+    #ingests both raw and analyzed (suffixes, etc.) versions of below fields
+    #this allows for more complex searchability, but blows up db size
     term = String(fields={'raw': String(index='not_analyzed')})
     building = String(fields={'raw': String(index='not_analyzed')})
-    location = GeoPoint(lat_lon=True)
     description = String(fields={'raw': String(index='not_analyzed')})
     meter = String(fields={'raw': String(index='not_analyzed')})
-    usage = Float()
 
     class Meta:
         index = 'meter'
@@ -56,7 +67,7 @@ with open('campus_buildings_geo_meters_data.csv','rb') as csvfile:
             except:
                 pass
             if usage=="" or usage==0:
-                print str(count)+": Skipped (usage is 0 or empty)."
+                print(str(count)+": Skipped (usage is 0 or empty).")
                 continue
 
             naive_date_str, _, offset_str = row[1].replace("+"," +").rpartition(' ')
@@ -68,9 +79,6 @@ with open('campus_buildings_geo_meters_data.csv','rb') as csvfile:
             dt = naive_dt.replace(tzinfo=FixedOffset(offset))
 
             reading = Reading()
-            #         0,                  1,            2,  3,             4,            5,                           6,    7
-            # 079039684,2014-03-12 00:30:00,"Winter 2014",A71,-87.6034113871,41.7893116662,"Bernard Mitchell Hospitial",484.2
-            #
             reading.meter=row[0]
             reading.datetime=dt
             reading.term=row[2]
